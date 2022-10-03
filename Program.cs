e1cbc66476923ace5056bbb53c887e649d9f2eb7
Vladimir Chuprov <xxazxxaz@yandex.ru>
2022-10-03 15:55:44 +0300

    Create new array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[4] {"hello", "2","world", ":)"};
+string[] array2 = new string[array1.Length];
